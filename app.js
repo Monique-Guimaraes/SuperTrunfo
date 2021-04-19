0c4131fdc6590e9a03bbcb981be50c2daf50b59e
Monique-Guimaraes <monique.pguimaraes@gmail.com>
2021-04-19 20:17:47 -0300

    Projeto supertrunfo do imersãodev da alura

---
+++ app.js
@@ -0,0 +1,358 @@
+var cartaMarin = {
+    nome: "Marin de Águia",
+    imagem: "https://pa1.narvii.com/6705/24eda4e1a1ff5f3f1ab34be862c84e09768c9487_hq.gif",
+    atributos: {
+       Soco: 384,
+       Chute: 462,
+       Técnica: 98,
+       Conhecimentos: 199,
+       Força: 184,
+    }
+}
+
+var cartaJango = {
+    nome: "Jango - Líder Cavaleiros Negros",
+    imagem: "https://static.wikia.nocookie.net/saintseya/images/c/c2/Chamas_do_Inferno_da_Ilha_da_Rainha_da_Morte.gif/revision/latest/scale-to-width-down/340?cb=20180320003826&path-prefix=pt",
+    atributos: {
+       Soco: 400,
+       Chute: 400,
+       Técnica: 300,
+       Conhecimentos: 300,
+       Força: 500,
+    }
+}
+
+var cartaShaka = {
+    nome: "Shaka de Virgem",
+    imagem: "https://64.media.tumblr.com/0d96b7d45bfc424bb2496d232013eb90/tumblr_nfezsa7Yp31rccjhpo1_500.gifv",
+    atributos: {
+       Soco: 432,
+       Chute: 301,
+       Técnica: 200,
+       Conhecimentos: 500,
+       Força: 500,
+    }
+}
+
+var cartaAiolos = {
+    nome: "Aiolos de Sagitário",
+    imagem: "https://pa1.narvii.com/6366/fca8588d1b44eec0cfe81e632d62fe64eb3b3d6e_hq.gif",
+    atributos: {
+       Soco: 499,
+       Chute: 492,
+       Técnica: 490,
+       Conhecimentos: 203,
+       Força: 494,
+    }
+}
+
+var cartaMascara = {
+    nome: "Máscara da Morte de Câncer",
+    imagem: "https://38.media.tumblr.com/tumblr_m7ul19rihA1r1n5pqo1_500.gif",
+    atributos: {
+       Soco: 397,
+       Chute: 398,
+       Técnica: 99,
+       Conhecimentos: 475,
+       Força: 305,
+    }
+}
+
+var cartaShiva = {
+    nome: "Shiva de Pavão",
+    imagem: "https://38.media.tumblr.com/tumblr_m7ul19rihA1r1n5pqo1_500.gif",
+    atributos: {
+       Soco: 300,
+       Chute: 300,
+       Técnica: 400,
+       Conhecimentos: 400,
+       Força: 400,
+    }
+}
+
+var cartaAioria = {
+    nome: "Aiolia de Leão",
+    imagem: "https://pa1.narvii.com/6399/f1315d7d77e7bb62e9b99cf81244fabad51f6602_hq.gif",
+    atributos: {
+       Soco: 499,
+       Chute: 207,
+       Técnica: 210,
+       Conhecimentos: 230,
+       Força: 105,
+    }
+}
+
+var cartaCamus = {
+    nome: "Camus de Aquário",
+    imagem: "https://64.media.tumblr.com/293eb4aa0a23e53275c27ce6ba3be17a/tumblr_no1irdO60O1saazguo1_540.gifv",
+    atributos: {
+       Soco: 499,
+       Chute: 220,
+       Técnica: 163,
+       Conhecimentos: 499,
+       Força: 315,
+    }
+}
+
+var cartaMiro = {
+    nome: "Miro de Escorpião",
+    imagem: "https://i.pinimg.com/originals/5f/69/64/5f696467865939c060e8b37d6863f56d.gif",
+    atributos: {
+       Soco: 487,
+       Chute: 302,
+       Técnica: 307,
+       Conhecimentos: 481,
+       Força: 295,
+    }
+}
+
+var cartaDante = {
+    nome: "Dante de Cérbero",
+    imagem: "https://pa1.narvii.com/6660/e47253eae82733c55fef906ccb74250c60e18126_hq.gif",
+    atributos: {
+       Soco: 295,
+       Chute: 302,
+       Técnica: 204,
+       Conhecimentos: 201,
+       Força: 309,
+    }
+}
+
+var cartaSeiya = {
+    nome: "Seiya de Pégaso",
+    imagem: "https://pa1.narvii.com/6200/23d81042e9c237f596a9ba6ef996a379e39dc48b_hq.gif",
+    atributos: {
+       Soco: 405,
+       Chute: 315,
+       Técnica: 207,
+       Conhecimentos: 315,
+       Força: 410,
+    }
+}
+
+var cartaShun = {
+    nome: "Shun de Andrômeda",
+    imagem: "http://4.bp.blogspot.com/-lL7Toh3S5ZA/U35N_Qhtw9I/AAAAAAAAHlQ/xOULD40GgHg/s1600/Shun+de+Andr%C3%B4meda+2.gif",
+    atributos: {
+       Soco: 198,
+       Chute: 199,
+       Técnica: 498,
+       Conhecimentos: 398,
+       Força: 202,
+    }
+}
+
+var cartaShura = {
+    nome: "Shura de Capricórnio",
+    imagem: "https://i.pinimg.com/originals/5b/02/ba/5b02ba444dfea112288cbcf3688ff094.gif",
+    atributos: {
+       Soco: 498,
+       Chute: 294,
+       Técnica: 115,
+       Conhecimentos: 102,
+       Força: 490,
+    }
+}
+
+var cartaAfrodite = {
+    nome: "Afrodite de Peixes",
+    imagem: "https://i.pinimg.com/originals/c6/73/02/c6730245f23a066935a8e9434b36fd5f.gif",
+    atributos: {
+       Soco: 301,
+       Chute: 298,
+       Técnica: 469,
+       Conhecimentos: 463,
+       Força: 217,
+    }
+}
+
+var cartaIkki = {
+    nome: "Ikki de Fênix",
+    imagem: "https://i.pinimg.com/originals/9f/15/77/9f1577f5c938eaffc8f781876797cc95.gif",
+    atributos: {
+       Soco: 498,
+       Chute: 205,
+       Técnica: 403,
+       Conhecimentos: 398,
+       Força: 320,
+    }
+}
+
+var cartaAldebaran = {
+    nome: "Aldebaran de Touro",
+    imagem: "https://pa1.narvii.com/6632/7dca8441cb34054926b07c9a5c907777b476d2f4_hq.gif",
+    atributos: {
+       Soco: 475,
+       Chute: 197,
+       Técnica: 99,
+       Conhecimentos: 298,
+       Força: 499,
+    }
+}
+
+var cartaMu = {
+    nome: "Mu de Áries",
+    imagem: "https://i.pinimg.com/originals/f4/46/88/f44688b2e42206f0bfccf94e36e847fb.gif",
+    atributos: {
+       Soco: 340,
+       Chute: 222,
+       Técnica: 315,
+       Conhecimentos: 499,
+       Força: 499,
+    }
+}
+
+var cartaSaga = {
+    nome: "Saga de Gêmeos",
+    imagem: "https://i.pinimg.com/originals/d1/2b/88/d12b88624c8e666ecbf797d483665654.gif",
+    atributos: {
+       Soco: 499,
+       Chute: 237,
+       Técnica: 249,
+       Conhecimentos: 491,
+       Força: 496,
+    }
+}
+
+var cartaShiryu = {
+    nome: "Shiryu de Dragão",
+    imagem: "https://i.pinimg.com/originals/a2/65/b3/a265b39e9c7d4692fd30777399da912e.gif",
+    atributos: {
+       Soco: 410,
+       Chute: 205,
+       Técnica: 420,
+       Conhecimentos: 230,
+       Força: 304,
+    }
+}
+
+var cartaHyoga = {
+    nome: "Hyoga de Cisne",
+    imagem: "http://4.bp.blogspot.com/-F_d7yOXHI7g/U35KvKEnbvI/AAAAAAAAHjk/Qeir0sFmnJ4/s1600/Yoga+2.gif",
+    atributos: {
+       Soco: 498,
+       Chute: 307,
+       Técnica: 208,
+       Conhecimentos: 205,
+       Força: 207,
+    }
+}
+
+var cartaShaina = {
+    nome: "Shaina de Ofiúco",
+    imagem: "https://pa1.narvii.com/7102/c4471828915e44c128aa53ef20c16af1a182d671r1-500-326_hq.gif",
+    atributos: {
+       Soco: 400,
+       Chute: 300,
+       Técnica: 100,
+       Conhecimentos: 205,
+       Força: 100,
+    }
+}
+
+var cartaAthena = {
+    nome: "Athena",
+    imagem: "https://i.pinimg.com/originals/76/61/54/766154a3b5167674eac48238aa2ff5fd.gif",
+    atributos: {
+       Soco: 60,
+       Chute: 60,
+       Técnica: 101,
+       Conhecimentos: 498,
+       Força: 98,
+    }
+}
+
+var cartaDohko = {
+    nome: "Dohko de Libra",
+    imagem: "https://i.pinimg.com/originals/3e/87/24/3e87241db1495de731dfea70cced115a.gif",
+    atributos: {
+       Soco: 498,
+       Chute: 498,
+       Técnica: 499,
+       Conhecimentos: 499,
+       Força: 401,
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaMarin, cartaJango, cartaShaka, cartaAiolos, cartaMascara, cartaShiva, cartaAioria, cartaCamus, cartaMiro, cartaDante, cartaSeiya, cartaShun, cartaShura, cartaAfrodite, cartaIkki, cartaAldebaran, cartaMu, cartaSaga, cartaShiryu, cartaHyoga, cartaShaina, cartaAthena, cartaDohko]
+
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 23)
+    cartaMaquina = cartas[numeroCartaMaquina]
+
+    var numeroCartaJogador = parseInt(Math.random() * 23)
+    while (numeroCartaJogador == numeroCartaMaquina) {
+        numeroCartaJogador = parseInt(Math.random() * 23)
+    }
+    cartaJogador = cartas[numeroCartaJogador]
+   
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+  
+    exibeCartaJogador()
+   
+}
+
+function exibeCartaJogador(){
+  var divCartaJogador = document.getElementById("carta-jogador")
+  var moldura = ' <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>` 
+  var opcoesTexto = ""
+  
+  for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+   
+  
+  var html = "<div id='opcoes' class= 'carta-status'>"
+  
+  divCartaJogador.innerHTML = moldura+nome+html+opcoesTexto+'</div>'
+  
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>' 
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>' 
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>' 
+    }
+    
+  divResultado.innerHTML = htmlResultado
+  exibeCartaMaquina()
+}
+
+function exibeCartaMaquina(){
+   var divCartaMaquina = document.getElementById("carta-maquina")
+   var moldura = ' <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>` 
+  var opcoesTexto = ""
+  
+  for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+   
+  
+  var html = "<div id='opcoes' class= 'carta-status'>"
+  
+  divCartaMaquina.innerHTML = moldura+nome+html+opcoesTexto+'</div>'
+  
+}
